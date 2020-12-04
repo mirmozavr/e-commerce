5028eb31a41ac951892f1e112afbc259ba455c92
Mo <mirmozavr@gmail.com>
2020-12-04 23:32:17 +0300

    Cart added

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from .views import BaseView, ProductDetailView, CategoryDetailView
+from .views import BaseView, ProductDetailView, CategoryDetailView, CartView, Customer
 urlpatterns = [
     path('', BaseView.as_view(), name='base'),
     path('products/<str:ct_model>/<str:slug>', ProductDetailView.as_view(), name='product_detail'),
-    path('category/<str:slug>', CategoryDetailView.as_view(), name='category_detail')
+    path('category/<str:slug>', CategoryDetailView.as_view(), name='category_detail'),
+    path('cart/', CartView.as_view(), name='cart')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, View
 
-from .models import Notebook, Smartphone, Category, LatestProducts
+from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart
 
 
 class BaseView(View):
@@ -39,3 +39,16 @@ class CategoryDetailView(DetailView):
     context_object_name = 'category'
     template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
+
+
+class CartView(View):
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
+        # cart = 'full cart'
+        categories = Category.objects.get_categories_for_left_sidebar()
+        context = {
+            'cart': cart,
+            'categories': categories
+        }
+        return render(request, 'cart.html', context)
